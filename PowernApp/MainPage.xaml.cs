cc6b76fa1f444ae0da16e49631f68f90e19fed44
Benjamin Sautermeister <apps@bsautermeister.de>
2013-11-22 20:26:37 -0500

    Added basic voice command framework, which currently raises an exception on startup due to an error in the SpeechRecognizer of the framework

--- PowernApp/MainPage.xaml.cs
+++ PowernApp/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using PowernApp.Resources;
+using PhoneKit.Framework.Voice;
 
 namespace PowernApp
 {
@@ -23,7 +24,113 @@ namespace PowernApp
         {
             InitializeComponent();
 
+            Loaded += (s, e) =>
+                {
+                    // register voice commands
+                    Speech.Instance.InstallCommandSets(new Uri("ms-appx:///voicecommands.xml", UriKind.Absolute));
+                };
+
             BuildLocalizedApplicationBar();
+        }
+
+        /// <summary>
+        /// When main page gets active, disables idle detection (to not interrupt the speech)
+        /// and try to parse voce commands from query string.
+        /// </summary>
+        /// <param name="e">The navigation args.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            // disable idle detection
+            PhoneApplicationService.Current.UserIdleDetectionMode =
+                IdleDetectionMode.Disabled;
+
+            try
+            {
+                String commandName = NavigationContext.QueryString["voiceCommandName"];
+
+                if (!string.IsNullOrEmpty(commandName))
+                    handleVoiceCommands(commandName);
+
+                // clear the QueryString or the page will retain the current value
+                NavigationContext.QueryString.Clear();
+            }
+            catch (Exception)
+            {
+                // this code block is reached if the app is accessed in a way other than voice commands, therefore, do nothing
+            }
+        }
+
+        /// <summary>
+        /// Handles the voice commands.
+        /// </summary>
+        /// <param name="commandName">The command name.</param>
+        private void handleVoiceCommands(string commandName)
+        {
+            string minutes;
+            switch (commandName)
+            {
+                case "startNap":
+                    minutes = NavigationContext.QueryString["minute"];
+                    handleStartNapCommand(minutes);
+                    break;
+                case "stopNap":
+                    handleStopNapCommand();
+                    break;
+                case "checkAlarmTime":
+                    handleCheckAlarmTime();
+                    break;
+                case "checkRemainingTime":
+                    handleCheckRemainingTime();
+                    break;
+                case "extendAlarmTime":
+                    minutes = NavigationContext.QueryString["minute"];
+                    handleExtendAlarmTime(minutes);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handles the start nap command.
+        /// </summary>
+        /// <param name="minutes">The length of the nap in minutes.</param>
+        private async void handleStartNapCommand(string minutes)
+        {
+            await Speech.Instance.Synthesizer.SpeakTextAsync("Have a nice " + minutes + " minutes nap.");
+        }
+
+        /// <summary>
+        /// Handles the stop nap command.
+        /// </summary>
+        private async void handleStopNapCommand()
+        {
+            await Speech.Instance.Synthesizer.SpeakTextAsync("Alarm is now off. Hope you feel more energized now.");
+        }
+
+        /// <summary>
+        /// Handles the check alarm time command.
+        /// </summary>
+        private async void handleCheckAlarmTime()
+        {
+            await Speech.Instance.Synthesizer.SpeakTextAsync("Alarm is set for 7:30 pm");
+        }
+
+        /// <summary>
+        /// Handles the check remaining time command.
+        /// </summary>
+        private async void handleCheckRemainingTime()
+        {
+            await Speech.Instance.Synthesizer.SpeakTextAsync("You have 9 minutes left.");
+        }
+
+        /// <summary>
+        /// Handles the extend alarm time command.
+        /// </summary>
+        /// <param name="minutes">The minutes to extend the alarm.</param>
+        private async void handleExtendAlarmTime(string minutes)
+        {
+            await Speech.Instance.Synthesizer.SpeakTextAsync("Done. You have now 14 minutes left. Happy napping.");
         }
 
         /// <summary>
