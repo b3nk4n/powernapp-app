bfd9335c8ccbc305388f1f0b1f2471a941235840
bsautermeister <apps@bsautermeister.de>
2014-09-10 11:10:56 +0200

    Changed version number

--- PowernApp/Resources/AppResources1.Designer.cs
+++ PowernApp/Resources/AppResources1.Designer.cs
@@ -169,7 +169,7 @@ namespace PowernApp.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 2.3 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 2.3.1 ähnelt.
         /// </summary>
         public static string ApplicationVersion {
             get {
